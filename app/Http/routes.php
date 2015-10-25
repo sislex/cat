3bacf7729dbf17624f92bf25a177b2fe6efbbfb6
sislex <sislex@ya.ru>
2015-10-25 17:58:40 +0300

    added Controllers/Admin/IndexController.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,11 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('admin/index', 'Admin\IndexController@index');
+
 
 // Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::get('admin/login', 'Auth\AuthController@getLogin');
+Route::post('admin/login', 'Auth\AuthController@postLogin');
+Route::get('admin/logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
