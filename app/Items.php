aee7aaabf279e6eb4669eb3aaf83a3add329fd7f
vit <vloseu@gmail.com>
2015-10-31 19:00:40 +0300

    Added items list view and item update ('Каталог Авто').

---
+++ app/Items.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Items extends Model
+{
+    protected $fillable = [
+        'name',
+        'title',
+        'description',
+        'keywords',
+        'short_text',
+        'text',
+        'obj'
+    ];
+}
