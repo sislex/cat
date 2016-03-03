99a4e069f41aa7826e412315fdfa7f093d11d381
sislex <sislex@ya.ru>
2016-03-03 11:50:31 +0300

    small fix

--- app/Items.php
+++ app/Items.php
@@ -30,6 +30,7 @@ class Items extends Model
             $model = isset($obj['type_auto'][0]['children'][0]['children'][0]['text'])?$obj['type_auto'][0]['children'][0]['children'][0]['text']:'';
             $god = isset($obj['God_vypuska'][0]['text'])?$obj['God_vypuska'][0]['text']:''. 'г.';
             $toplivo = isset($obj['Тип двигателя'][0]['text'])?$obj['Тип двигателя'][0]['text']:'';
+
             if($input['price']){
                 $input['price'] = intval($input['price']);
                 $price = "Цена {$input['price']}$";
